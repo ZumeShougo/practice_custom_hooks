e8605ba08250423765cf964e3af6a494343f3f58
ZumeShougo <g0109725rena@gmail.com>
2023-02-10 13:56:55 +0900

    Add users component

--- src/User.js
+++ src/User.js
@@ -1,1 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from 'react';
+
+const User = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const users = await response.json();
+      setUsers(users);
+    };
+    fetchUser();
+  }, []);
+  return(
+    <div>
+      <h1>ユーザー一覧</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
